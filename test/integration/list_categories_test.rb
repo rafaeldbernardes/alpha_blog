cd0b1f852b3109ce510c33b90d194833864a9a14
rafaeldelbel <rafaeldbernardes@gmail.com>
2016-09-03 12:01:48 -0300

    Add categories model and tests

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @category = Category.create(name: 'sports')
+    @category_2 = Category.create(name: 'programming')
+  end
+
+  test "should show categories listing" do
+    get categories_path
+    assert_template 'categories/index'
+    assert_select 'a[href=?]', category_path(@category), text: @category.name
+    assert_select 'a[href=?]', category_path(@category_2), text: @category_2.name
+  end
+end
